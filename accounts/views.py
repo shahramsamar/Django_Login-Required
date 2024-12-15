fe6ba157e443b4a47fe72eed32ee45f9fa1b8fd9
shahramsamar <shahramsamar2010@gmail.com>
2024-12-15 15:33:40 +0330

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.views.generic import TemplateView
 
 
 
-class WelcomeView(LoginRequiredMixin, TemplateView):
+class WelcomeView(PermissionRequiredMixin, LoginRequiredMixin, TemplateView):
     """
     Displays the welcome page after login.
     """
+    #for implementation permissions
+    permission_required ='accounts.view_login'
     # login_url = '/accounts/login/'  # Corrected login URL
-    template_name = 'welcome.html'
+    template_name = 'registration/welcome.html'
